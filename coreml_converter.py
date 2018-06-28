be90385cd465a41f2a35d9a4bfa5ed80eeb391bc
LucianoAlmeida <passos.luciano@outlook.com>
2018-06-28 03:31:21 +0000

    Generating a model with label output

--- coreml_converter.py
+++ coreml_converter.py
@@ -3,4 +3,5 @@ tf_converter.convert(tf_model_path = 'mnist_model.pb',
                      mlmodel_path = 'mnist_model.mlmodel',
                      input_name_shape_dict= {'x:0': [1, 28, 28, 1], 'y:0': [1, 10] },
                      output_feature_names = ['predictions:0'],
+                     class_labels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
                      image_input_names='x:0')
