7e4a6e437e0f806e1dd70bd8652f3e0fe93519dc
LucianoAlmeida <passos.luciano@outlook.com>
2018-06-28 02:57:47 +0000

    Generating a new model

--- coreml_converter.py
+++ coreml_converter.py
@@ -2,4 +2,5 @@ import tfcoreml as tf_converter
 tf_converter.convert(tf_model_path = 'mnist_model.pb',
                      mlmodel_path = 'mnist_model.mlmodel',
                      output_feature_names = ['predictions:0'],
+                     image_input_names='image',
                      input_name_shape_dict = {'x:0' : [1, 28, 28, 1], 'y:0': [1, 10]})
