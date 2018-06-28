48ba2ee2f5969f481fb39af5c6a7351ed5d712b1
LucianoAlmeida <passos.luciano@outlook.com>
2018-06-28 07:12:09 +0000

    Renamimg CoreML Model

--- coreml_converter.py
+++ coreml_converter.py
@@ -1,6 +1,6 @@
 import tfcoreml as tf_converter
 tf_converter.convert(tf_model_path = 'mnist_model.pb',
-                     mlmodel_path = 'mnist_model.mlmodel',
+                     mlmodel_path = 'Mnist.mlmodel',
                      input_name_shape_dict= {'x:0': [1, 28, 28, 1], 'y:0': [1, 10] },
                      output_feature_names = ['predictions:0'],
                      class_labels=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
