7ff07e05418916162fed95f60e3a06e62718f387
LucianoAlmeida <passos.luciano@outlook.com>
2018-06-28 03:15:29 +0000

    Generating a model with image input

--- coreml_converter.py
+++ coreml_converter.py
@@ -1,6 +1,6 @@
 import tfcoreml as tf_converter
 tf_converter.convert(tf_model_path = 'mnist_model.pb',
                      mlmodel_path = 'mnist_model.mlmodel',
+                     input_name_shape_dict= {'x:0': [1, 28, 28, 1], 'y:0': [1, 10] },
                      output_feature_names = ['predictions:0'],
-                     image_input_names='image',
-                     input_name_shape_dict = {'x:0' : [1, 28, 28, 1], 'y:0': [1, 10]})
+                     image_input_names='x:0')
